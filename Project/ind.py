00359dd012922f3a920c15b6803806cf7ec6ea9b
dshayderov <dshayderov@list.ru>
2022-10-17 22:48:05 +0300

    add ind

---
+++ Project/ind.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def list_min(li):
+    if len(li) == 2:
+        if li[1] < li[0]:
+            return li[1]
+        else:
+            return li[0]
+    else:
+        minim = list_min(li[1:])
+        if li[0] < minim:
+            return li[0]
+        else:
+            return minim
+
+
+if __name__ == '__main__':
+    sp = list(map(float, input("Введите список чисел: ").split(' ')))
+    print(list_min(sp))
